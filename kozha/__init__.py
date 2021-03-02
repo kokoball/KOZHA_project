6abeaf1062b88645b2d2a29a749aa9c12d314901
babywalnut <anfuza@naver.com>
2021-03-02 14:43:31 +0900

    profile기능추가, 비밀번호찾기 기능 추가

--- kozha/__init__.py
+++ kozha/__init__.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 # 서버가 처음 시작되었을 때 코드가 실행되는 부분 
 app = Flask(__name__)
@@ -21,6 +23,12 @@ bcrypt = Bcrypt(app) #해쉬값 생성
 login_manager = LoginManager(app)
 login_manager.login_view='login'
 login_manager.login_message_category='info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'anfuza@likelion.org'
+app.config['MAIL_PASSWORD'] = ''
+mail = Mail(app)
 
 
 
